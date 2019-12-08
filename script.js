798447f14757d350c03c59709a60c64f5ff733c9
AdamSkaznik <46684800+AdamSkaznik@users.noreply.github.com>
2019-12-08 15:04:59 +0100

    dzień6

--- script.js
+++ script.js
@@ -24,42 +24,9 @@ function dodaj() {
          sumCount = sumCount + cena;
          console.log(sumCount);
          document.getElementById('sumCount').innerHTML = sumCount +" PLN";
-        /* c0 = document.querySelector('.price');
-         c1 = c0.innerText;
-         console.log(c1);
-         let t1 = document.createElement('tr');
-         t1.classList.add('tr1');
-         t1.setAttribute("id", "tr1");
-         document.getElementById('table').appendChild(t1);
-         console.log(t1);
-        let td1 = document.createElement('td');
-        td1.classList.add('td1');
-        td1.innerHTML = "test"
-        td1.setAttribute("id", "td1");
-        document.getElementById('tr1').appendChild(td1);
-        console.log(td1);
-         let td2 = document.createElement('td');
-         td2.classList.add('td2');
-         td2.setAttribute("id", "td2");
-         document.getElementById('tr1').appendChild(td2);
-         console.log(td2);
-         let t2 = document.createElement('tr');
-         t2.classList.add('tr2');
-         t2.setAttribute("id", "tr2");
-         document.getElementById('table').appendChild(t2);
-         console.log(t2);
-         let td3 = document.createElement('td');
-         td3.classList.add('td3');
-         td3.innerHTML = " Cena :"+cena;
-         td3.setAttribute("id", "td3");
-         document.getElementById('tr2').appendChild(td3);
-         console.log(td3);
-         let tf1 = document.createElement('tfoot');
-         tf1.classList.add('tf1');
-         tf1.setAttribute("id", "tf1");
-         document.getElementById('tr2').appendChild(tf1);*/
          boxCount++;
          console.log(boxCount);
+
      } else {
          alert('Możesz kupić tylko 8');
      }
